07b4f5d01c04f8b0bfdd8dd4a14be4aaee4ac759
Felipex77 <dias20felipe@gmail.com>
2025-02-24 19:58:19 +0000

    exercicio concluido

---
+++ estrutura condicional/exercicios/5.exercicio_login.py
@@ -0,0 +1,13 @@
+email = "Felipe dias"
+senha = "123"
+
+
+
+login = input("Digite seu Email")
+senha_login = input("Digite sua senha")
+
+if email == login and senha == senha_login:
+ print("Acesso permitido.")
+else:
+ print("Acesso negado") 
+
