a118ac1bbb582a15d20be49322eddf0e16a3529a
Felipex77 <dias20felipe@gmail.com>
2025-04-02 19:57:06 +0000

    opeacoes mateamaticas

---
+++ função/exercicio_funcao/2.funcao_subtracao.py
@@ -0,0 +1,32 @@
+def multiplicacao(n1,n2):
+   
+    return n1 * n2
+n1 = int(input("Digite um numero:"))
+n2 = int(input("Digite um numero:")) 
+
+multiplicacaos = multiplicacao(n1, n2)
+
+print(multiplicacaos)
+
+
+def divisao(n1,n2):
+   
+    return n1 / n2
+n1 = int(input("Digite um numero:"))
+n2 = int(input("Digite um numero:")) 
+
+divisaos = divisao(n1, n2)
+
+print(divisao)
+
+def subtracao(n1,n2):
+   
+    return n1 - n2
+n1 = int(input("Digite um numero:"))
+n2 = int(input("Digite um numero:")) 
+
+subtracaos= subtracao(n1, n2)
+
+print(subtracaos)
+
+
